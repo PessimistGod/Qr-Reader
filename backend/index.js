3a60be357c887ff8082bc31b08f93fa6729337fa
PessimistGod <rameshrahul0004@gmail.com>
2023-10-23 10:54:43 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,9 +12,8 @@ app.use(express.json());
 
 
 
-app.use(cors())
+app.use(cors({ origin: '*' }));
 
-app.options("*", cors())
 
 const pool = new Pool({
     connectionString: process.env.POSTGRES_URL + "?sslmode=require",
